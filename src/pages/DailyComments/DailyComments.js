a90441a78a781a02647c9bee433a363b99c31874
Mase Santos <hola@masesantos.work>
2020-07-18 21:54:39 -0500

    Created app pages

---
+++ src/pages/DailyComments/DailyComments.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DailyComments extends Component {
+    render() {
+        return (
+            <>
+
+            </>
+        );
+    }
+}
+
+export default DailyComments;
---
+++ src/pages/DailySupport/DailySupport.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DailySupport extends Component {
+    render() {
+        return (
+            <>
+
+            </>
+        );
+    }
+}
+
+export default DailySupport;
