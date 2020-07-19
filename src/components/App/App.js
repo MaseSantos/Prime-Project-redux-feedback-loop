b7579553a14890a49c753421d66c81cc5ba91fb1
Mase Santos <hola@masesantos.work>
2020-07-18 21:43:10 -0500

    Created a header for the app and sourced it into App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Header from '../Header/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
+        <>
+        <Header/>
+        <h4><i>Insert Instructions here</i></h4>
         <br/>
-      </div>
+      </>
     );
   }
 }
