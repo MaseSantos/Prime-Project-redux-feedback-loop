b0ee60f570cb46ecf5dba5ef877671cb515678ce
Mase Santos <hola@masesantos.work>
2020-07-19 16:47:14 -0500

    Created 'CommentsButton' to move from 'DailySupport' to the 'DailyComments' page

--- src/pages/DailySupport/DailySupport.js
+++ src/pages/DailySupport/DailySupport.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import UnderstandingButton from "../../components/buttons/CommentsButton/CommentsButton";
+import CommentsButton from '../../components/buttons/CommentsButton/CommentsButton';
+
 
 class DailySupport extends Component {
     render() {
         return (
             <>
                 <h4>How well are you being supported?</h4>
+                <CommentsButton/>
             </>
         );
     }
