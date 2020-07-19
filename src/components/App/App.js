62331c283326d9abcd012248a8c44926244b79ee
Mase Santos <hola@masesantos.work>
2020-07-19 15:55:14 -0500

    Created select tag component for user to rate how they feel for each question

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Header from '../Header/Header';
+import SelectTag from '../SelectTag/SelectTag';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
         <>
         <Header/>
         <h4><i>Insert Instructions here</i></h4>
+        <SelectTag/>
         <br/>
       </>
     );
