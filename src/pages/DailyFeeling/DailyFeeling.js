f5bb436036b623146d3654ada66ad3102627f1d8
Mase Santos <hola@masesantos.work>
2020-07-19 16:40:21 -0500

    Created 'Understanding Button' to move from 'DailyFeeling' to the 'DailyUnderstanding' page

--- src/pages/DailyFeeling/DailyFeeling.js
+++ src/pages/DailyFeeling/DailyFeeling.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import UnderstandingButton from "../../components/buttons/UnderstandingButton/UnderstandingButton";
 
 class DailyFeeling extends Component {
     render() {
         return (
             <>
                 <h4>How are you feeling today?</h4>
+                <UnderstandingButton/>
             </>
         );
     }
